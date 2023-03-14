b04caa53553c5de46e30a621c7b77396ce9b646d
jb <101614021+jbc2313@users.noreply.github.com>
2023-03-14 14:12:00 -0500

    added player pos

--- main.c
+++ main.c
@@ -4,15 +4,29 @@
 
 // Program setup to run on Linux(Fedora)
 
+// Player Posistion
+float px,py;  
+
+void drawPlayer()  {
+    glColor3f(1,1,0);
+    glPointSize(8);
+    glBegin(GL_POINTS);
+    glVertex2i(px,py);
+    glEnd();
+}
+
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    drawPlayer();
     glutSwapBuffers();
 };
 
 void init() {
     glClearColor(0.3,0.3,0.3,0);
     gluOrtho2D(0,1024,512,0);
+    px=300;
+    py=300;
 };
 
 int main(int argc, char *argv[]) {
